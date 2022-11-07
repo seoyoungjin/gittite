020b5c770bf0320b42f3a5e6ae36950737044bbb
Seo Youngjin <yjinseo@gmail.com>
2022-11-07 11:28:15 +0900

    welcome page

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,22 +1,23 @@
 <script setup lang="ts">
 import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
+
 import CommunityIcon from './icons/IconCommunity.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
 import SupportIcon from './icons/IconSupport.vue'
 </script>
 
 <template>
   <WelcomeItem>
     <template #icon>
-      <DocumentationIcon />
+      <CommunityIcon />
     </template>
-    <template #heading>Documentation</template>
+    <template #heading></template>
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
+    This is the place to test the function of git one by one.
+    The return of each command can be viewed directly in json format
+    or in a different layout.
   </WelcomeItem>
 
   <WelcomeItem>
@@ -25,62 +26,53 @@ import SupportIcon from './icons/IconSupport.vue'
     </template>
     <template #heading>Tooling</template>
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a>.
+    This project is based on tauri framework <code>git2-rs</code> library.
+    And most of code is copied from Gitui project.
+    <ul>
+      <li><a href="https://tauri.app/" target="_blank" rel="noopener">Tauri</a></li>
+      <li><a href="https://docs.rs/git2/latest/git2/" target="_blank" rel="noopener">git2-rs</a></li>
+      <li><a href="https://github.com/extrawurst/gitui" target="_blank" rel="noopener">Giuui</a></li>
+    </ul>
+    User interface is written with <code>Vue.js</code> and <code>Quasar</code>.
+    <ul>
+      <li><a href="https://vuejs.org/" target="_blank" rel="noopener">Vue</a>,</li>
+      <li><a href="https://quasar.dev/" target="_blank" rel="noopener">Quasar</a>,</li>
+    </ul>
+  </WelcomeItem>
 
-    <br />
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Documentaion</template>
 
-    More instructions are available in <code>README.md</code>.
+    Documentation will be composed of three parts.
+    <ul>
+      <li>program manual</li>
+      <li>javascript api description</li>
+      <li>rust code explanation</li>
+    </ul>
+    More instructions are available in <code>README.md</code>
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <EcosystemIcon />
     </template>
-    <template #heading>Ecosystem</template>
+    <template #heading>Test</template>
 
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
+    The test consists of a rust code unit test and a javascript case test.
+    I am planning to add more cases in the future.
+    <br />
+    Visit <code>test features</code> page.
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <SupportIcon />
     </template>
-    <template #heading>Support Vue</template>
+    <template #heading>Support</template>
 
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+    Any help is welcome.
   </WelcomeItem>
 </template>
