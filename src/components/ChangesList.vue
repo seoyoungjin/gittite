9aa1c0ea0cdb8cb40c7bcce7100220ceb1a7064e
Seo Youngjin <yjinseo@gmail.com>
2022-11-19 23:42:47 +0900

    quasar-extras-svg-icons/oct-icons-v17 added

--- src/components/ChangesList.vue
+++ src/components/ChangesList.vue
@@ -2,19 +2,20 @@
   <div class="q-ma-none">
     <!--Unstaged-->
     <div class="text-h7">Unstaged Changes</div>
-
-    <q-scroll-area style="height:25vh;">
+    <q-scroll-area style="height: 25vh">
       <q-list dense bordered padding class="rounded-borders">
         <q-item
           v-for="(item, index) in unstagedData"
           :key="index"
           clickable
-          v-ripple
           @click="clickItem(item)"
         >
           <q-item-section side>
-            <q-icon name="folder" color="amber" />
-            <!-- {{ item.wtree }} -->
+            <q-icon
+              :name="octIconForStatus(item.wtree)"
+              color="blue"
+              size="14px"
+            />
           </q-item-section>
           <q-item-section>
             <q-item-label>{{ item.path }}</q-item-label>
@@ -23,21 +24,23 @@
       </q-list>
     </q-scroll-area>
 
-    <q-scroll-area style="height:25vh;">
+    <!--Staged-->
+    <div class="text-h7">Staged Changes</div>
+    <q-scroll-area style="height: 25vh">
       <q-list dense bordered padding class="rounded-borders">
-        <q-item clickable v-ripple>
-          <q-item-section> Staged </q-item-section>
-        </q-item>
 
         <q-item
           v-for="(item, index) in stagedData"
           :key="index"
           clickable
-          v-ripple
+          @click="clickItem(item)"
         >
           <q-item-section side>
-            <q-icon name="folder" color="blue" />
-            <!-- {{ item.stage }} -->
+            <q-icon
+              :name="octIconForStatus(item.stage)"
+              color="amber"
+              size="14px"
+            />
           </q-item-section>
           <q-item-section>
             <q-item-label>{{ item.path }}</q-item-label>
@@ -45,7 +48,6 @@
         </q-item>
       </q-list>
     </q-scroll-area>
-
 
     <div>
       <ChangesOption />
@@ -55,9 +57,30 @@
 
 <script lang="ts">
 import ChangesOption from "@/components/ChangesOption.vue";
+import {
+  octDiff16,
+  octDiffAdded16,
+  octDiffIgnored16,
+  octDiffModified16,
+  octDiffRemoved16,
+  octDiffRenamed16,
+  octFileDiff16,
+} from "quasar-extras-svg-icons/oct-icons-v17";
 import * as git2rs from "@/api/git2rs";
 
 export default {
+  setup() {
+    return {
+      octDiff16,
+      octDiffAdded16,
+      octDiffIgnored16,
+      octDiffModified16,
+      octDiffRemoved16,
+      octDiffRenamed16,
+      octFileDiff16,
+    };
+  },
+
   components: {
     ChangesOption,
   },
@@ -80,8 +103,27 @@ export default {
         this.unstagedData = await git2rs.getStatus("workdir");
       })();
     },
+
     clickItem(item) {
       this.$emit("selectFile", item.path);
+    },
+
+    octIconForStatus(status: string): string {
+      switch (status) {
+        case "New":
+        case "Added":
+        case "Untracked":
+          return this.octDiffAdded16;
+        case "Modified":
+          return this.octDiffModified16;
+        case "Deleted":
+          return this.octDiffRemoved16;
+        case "Renamed":
+          return this.octDiffRenamed16;
+        // case "Conflicted":
+        default:
+          throw "Unknown status";
+      }
     },
   },
 };
